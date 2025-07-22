0d7276f395110b44895cf95878f909fbad42265a
Matheus Freitas Santos <mtfrsantos@gmail.com>
2025-07-22 16:58:29 -0300

    fix: remove neotree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -9,9 +9,9 @@
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
+require("lazy").setup({
     -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
     -- NOTE: Plugins can also be added by using a table,
     -- with the first argument being the link and the following
@@ -23,25 +23,25 @@ require('lazy').setup({
     -- modular approach: using `require 'path/name'` will
     -- include a plugin definition from file lua/path/name.lua
 
-    require 'kickstart/plugins/gitsigns',
-    require 'kickstart/plugins/which-key',
-    require 'kickstart/plugins/telescope',
-    require 'kickstart/plugins/lspconfig',
-    require 'kickstart/plugins/conform',
-    require 'kickstart/plugins/blink-cmp',
-    require 'kickstart/plugins/tokyonight',
-    require 'kickstart/plugins/todo-comments',
-    require 'kickstart/plugins/mini',
-    require 'kickstart/plugins/treesitter',
-    require 'kickstart/plugins/debug',
-    require 'kickstart/plugins/indent_line',
-    require 'kickstart/plugins/lint',
-    require 'kickstart/plugins/autopairs',
-    require 'kickstart/plugins/neo-tree',
+    require("kickstart/plugins/gitsigns"),
+    require("kickstart/plugins/which-key"),
+    require("kickstart/plugins/telescope"),
+    require("kickstart/plugins/lspconfig"),
+    require("kickstart/plugins/conform"),
+    require("kickstart/plugins/blink-cmp"),
+    require("kickstart/plugins/tokyonight"),
+    require("kickstart/plugins/todo-comments"),
+    require("kickstart/plugins/mini"),
+    require("kickstart/plugins/treesitter"),
+    require("kickstart/plugins/debug"),
+    require("kickstart/plugins/indent_line"),
+    require("kickstart/plugins/lint"),
+    require("kickstart/plugins/autopairs"),
+    -- require 'kickstart/plugins/neo-tree',
 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
-    { import = 'custom.plugins' },
+    { import = "custom.plugins" },
     -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
     -- Or use telescope!
     -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
@@ -51,19 +51,19 @@ require('lazy').setup({
         -- If you are using a Nerd Font: set icons to an empty table which will use the
         -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
         icons = vim.g.have_nerd_font and {} or {
-            cmd = '⌘',
-            config = '🛠',
-            event = '📅',
-            ft = '📂',
-            init = '⚙',
-            keys = '🗝',
-            plugin = '🔌',
-            runtime = '💻',
-            require = '🌙',
-            source = '📄',
-            start = '🚀',
-            task = '📌',
-            lazy = '💤 ',
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
         },
     },
 })
