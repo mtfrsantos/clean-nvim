b560b98b828b21ebb431da95f28da89b771dc16a
Matheus Freitas Santos <mtfrsantos@gmail.com>
2025-10-27 09:27:57 -0300

    fix: remove atac

--- lua/custom/plugins/atac.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'NachoNievaG/atac.nvim',
-    dependencies = { 'akinsho/toggleterm.nvim' },
-    config = function()
-        require('atac').setup {
-            dir = '~/atac',
-        }
-    end,
-}
