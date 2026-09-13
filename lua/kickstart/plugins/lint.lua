399aa9f80c6d3bbac0630635aee0c6afd15752e4
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-09-13 10:12:55 -0300

    feat: better lint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -1,11 +1,8 @@
--- Linting
-
 vim.pack.add({ "https://github.com/mfussenegger/nvim-lint" })
 
 local lint = require("lint")
 local uv = vim.uv or vim.loop
 
--- Configure linters by filetype
 lint.linters_by_ft = {
     python = { "pylint", "ruff", "mypy", "codespell" },
     markdown = { "markdownlint", "codespell" },
@@ -13,12 +10,10 @@ lint.linters_by_ft = {
     go = { "golangcilint", "codespell" },
 }
 
--- Pylint specific setup for Virtual Environments
 local pylint = lint.linters.pylint
 if pylint then
     pylint.args = {
         function()
-            -- All logic goes inside this function
             local venv = os.getenv("VIRTUAL_ENV") or (vim.fn.getcwd() .. "/.venv")
             local hook = ""
             if vim.fn.isdirectory(venv) == 1 then
@@ -39,6 +34,7 @@ if pylint then
             end
             return hook
         end,
+        "--disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,redefined-outer-name",
         "-f",
         "json",
         "--from-stdin",
@@ -60,5 +56,20 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         if vim.bo.modifiable then
             lint.try_lint()
         end
+    end,
+})
+
+-- Auto search mypy in virtual environment
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+    group = vim.api.nvim_create_augroup("Linting", { clear = true }),
+    callback = function()
+        local cwd = vim.fn.getcwd()
+        local venv_mypy = cwd .. "/.venv/bin/mypy"
+        if vim.fn.executable(venv_mypy) == 1 then
+            lint.linters.mypy.cmd = venv_mypy
+        else
+            lint.linters.mypy.cmd = "mypy"
+        end
+        lint.try_lint()
     end,
 })
